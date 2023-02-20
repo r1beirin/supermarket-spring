832d8c69167af1491328fab28d529f4bd1793053
r1beirin <mateusriibeiro@gmail.com>
2023-02-20 18:46:10 -0300

    homeController

---
+++ src/main/java/com/springstudie/supermarket/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.springstudie.supermarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("index");
+    }
+
+}
