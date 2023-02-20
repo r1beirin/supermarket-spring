c187fd143bc5b86f967fdf45f6c9622df69c8c58
r1beirin <mateusriibeiro@gmail.com>
2023-02-20 19:27:17 -0300

    create registerproduct

--- src/main/java/com/springstudie/supermarket/controller/ProductController.java
+++ src/main/java/com/springstudie/supermarket/controller/ProductController.java
@@ -3,14 +3,22 @@ package com.springstudie.supermarket.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ProductController {
 
-    @RequestMapping("/products")
+    @RequestMapping(value = "/products", method = RequestMethod.GET)
     public String seeProducts(Model model){
         model.addAttribute("products");
 
         return "products";
+    }
+
+    @RequestMapping(value = "/products/register", method = RequestMethod.GET)
+    public String registerProduct(Model model){
+        model.addAttribute("registerProduct");
+
+        return "registerProduct";
     }
 }
