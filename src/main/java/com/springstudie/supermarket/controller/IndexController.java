fc2e55139b0b60d4885e36f586329cf8240d63ed
r1beirin <mateusriibeiro@gmail.com>
2023-02-20 19:04:09 -0300

    rename indexcontroller

--- src/main/java/com/springstudie/supermarket/controller/IndexController.java
+++ src/main/java/com/springstudie/supermarket/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class HomeController {
+public class IndexController {
 
     @GetMapping("/")
     public ModelAndView index(){
