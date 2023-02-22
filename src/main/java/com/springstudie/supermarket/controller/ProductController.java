97caa7b0356b7f143b2e5c20234ce32d361dd476
r1beirin <mateusriibeiro@gmail.com>
2023-02-22 14:30:41 -0300

    testing products ModelAndView

--- src/main/java/com/springstudie/supermarket/controller/ProductController.java
+++ src/main/java/com/springstudie/supermarket/controller/ProductController.java
@@ -1,18 +1,34 @@
 package com.springstudie.supermarket.controller;
 
+import com.springstudie.supermarket.model.usecases.Product;
+import com.springstudie.supermarket.repository.ProductRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
+    private final ProductRepository productRepository;
 
-    @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public String seeProducts(Model model){
-        model.addAttribute("products");
+    private final ProductsAPIController productAPI;
 
-        return "products";
+    public ProductController(ProductRepository productRepository, ProductsAPIController productAPI1) {
+        this.productRepository = productRepository;
+        this.productAPI = productAPI1;
+    }
+    @GetMapping("/products")
+    public ModelAndView products(){
+        ModelAndView mv = new ModelAndView("products");
+        List<Product> productList = productAPI.getAllProducts();
+        System.out.println(productList);
+        //mv.addObject(productList);
+
+        return mv;
     }
 
     @RequestMapping(value = "/products/register", method = RequestMethod.GET)
