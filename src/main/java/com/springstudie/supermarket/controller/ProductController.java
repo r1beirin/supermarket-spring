be84b04bb4aa318e7915841872b9dee72f7a946a
r1beirin <mateusriibeiro@gmail.com>
2023-02-22 23:09:01 -0300

    first part of /product/edit

--- src/main/java/com/springstudie/supermarket/controller/ProductController.java
+++ src/main/java/com/springstudie/supermarket/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.springstudie.supermarket.controller;
 import com.springstudie.supermarket.model.usecases.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,5 +30,13 @@ public class ProductController {
     public ModelAndView registerProduct(){
 
         return new ModelAndView("productsRegister");
+    }
+
+    @GetMapping("/products/edit/{id}")
+    public ModelAndView editProduct(@PathVariable long id){
+        ModelAndView mv = new ModelAndView("productsRegister");
+        Product product = productAPI.getProduct(id).getBody();
+        mv.addObject("product", product);
+        return mv;
     }
 }
