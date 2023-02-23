ab4103b5b306d0447059b1c72dcdb9998c1aeced
r1beirin <mateusriibeiro@gmail.com>
2023-02-22 22:06:41 -0300

    remove inutil line

--- src/main/java/com/springstudie/supermarket/controller/ProductController.java
+++ src/main/java/com/springstudie/supermarket/controller/ProductController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.util.List;
 
 @Controller
