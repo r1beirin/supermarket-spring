f26a5dcea16a0fc8e3f4af6049a08ea7651a2832
r1beirin <mateusriibeiro@gmail.com>
2023-07-18 15:15:56 -0300

    feat: findByEmail search query

--- src/main/java/com/springstudie/supermarket/repository/UserRepository.java
+++ src/main/java/com/springstudie/supermarket/repository/UserRepository.java
@@ -2,7 +2,10 @@ package com.springstudie.supermarket.repository;
 
 import com.springstudie.supermarket.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findById(long id);
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    User findByEmail(@Param("email") String email);
 }
