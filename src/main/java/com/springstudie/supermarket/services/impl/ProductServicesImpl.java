f3abc9aea880962517273f868d9393a11a429347
r1beirin <mateusriibeiro@gmail.com>
2023-07-18 17:26:33 -0300

    docs: updating documentation

--- src/main/java/com/springstudie/supermarket/services/impl/ProductServicesImpl.java
+++ src/main/java/com/springstudie/supermarket/services/impl/ProductServicesImpl.java
@@ -15,20 +15,12 @@ import java.time.LocalDate;
 @Service
 public class ProductServicesImpl implements ProductServices {
 
-    /**
-     * This a method to verify if a product object is null.
-     * @param product Product object
-     * @return boolean
-     */
+    @Override
     public boolean isProductNotExists(Product product){
         return product == null;
     }
 
-    /**
-     * This a method to json request validator.
-     * @param json string
-     * @return boolean
-     */
+    @Override
     public boolean isValidJson(String json) {
         ObjectMapper mapper = new ObjectMapper()
                 .enable(DeserializationFeature.FAIL_ON_TRAILING_TOKENS);
@@ -41,11 +33,7 @@ public class ProductServicesImpl implements ProductServices {
         return true;
     }
 
-    /**
-     * This a method to convert a product object to json object.
-     * @param product Product object
-     * @param json JSONObject
-     */
+    @Override
     public void product2json(Product product, JSONObject json){
         json.put("nameProduct", product.getNameProduct());
         json.put("valueProduct", product.getValueProduct());
@@ -57,9 +45,8 @@ public class ProductServicesImpl implements ProductServices {
      * This method validate a field in json request.
      * Regex: [a-zA-Z\\s]+ -> to validate just word.
      * Regex: ^ *$ -> to validate wordspaces.
-     * @param product JSONObject object
-     * @return boolean
      */
+    @Override
     public boolean isValidField(JSONObject product){
         return  product.get("nameProduct") != null &&
                 product.get("nameProduct").toString().matches("[a-zA-Z\\s]+") &&
@@ -72,11 +59,7 @@ public class ProductServicesImpl implements ProductServices {
                 !product.get("expirationProductAt").toString().matches("^ *$");
     }
 
-    /**
-     * This method set a field in json request.
-     * @param product object
-     * @param productToBeConverted object
-     */
+    @Override
     public void setProductField(Product product, JSONObject productToBeConverted){
         String nameProduct = (String) productToBeConverted.get("nameProduct");
         var valueProduct = Double.parseDouble(String.valueOf(productToBeConverted.get("valueProduct")));
@@ -89,25 +72,24 @@ public class ProductServicesImpl implements ProductServices {
         product.setExpirationProductAt(LocalDate.parse(expirationProductAt));
     }
 
-    /**
-     * Below are methods to flag the status code on requests.
-     * @return ResponseEntity
-     */
+    @Override
     @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Some parameters are invalid")
     public ResponseEntity<Product> onIllegalArgumentMessage() {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
-
+    @Override
     @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not exist")
     public ResponseEntity<Product> onNotFoundMessage(){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
+    @Override
     @ResponseStatus(value = HttpStatus.OK, reason = "Successful")
     public ResponseEntity<Product> onSuccessMessage(){
         return ResponseEntity.ok().build();
     }
 
+    @Override
     @ResponseStatus(value = HttpStatus.OK, reason = "Successful")
     public ResponseEntity<Product> onSuccessMessage(Product product){
         return ResponseEntity.ok(product);
